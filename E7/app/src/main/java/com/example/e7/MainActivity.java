d3d47a1441b8cc2cf9d0568fa62801d2bb9359fe
kareemtawab <kareem.tawab@gmail.com>
2022-09-20 00:24:35 +0200

    E7 added

---
+++ E7/app/src/main/java/com/example/e7/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.e7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent in = new Intent (MainActivity.this, LoginActivity.class);
+                startActivity(in);
+                finish();
+            }
+        }, 4500);
+    }
+
+
+}
